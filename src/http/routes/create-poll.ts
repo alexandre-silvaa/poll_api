5ee70f1110b60f6fc71e7491bfb74439f3cbec6a
alexandre-silvaa <alexandrebsilva9@gmail.com>
2024-02-08 23:55:03 -0300

    create poll with options

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -6,12 +6,29 @@ export async function createPoll(app: FastifyInstance) {
   app.post("/polls", async (response, reply) => {
     const createPollBody = z.object({
       title: z.string(),
+      options: z.array(z.string()),
     });
-    const { title } = createPollBody.parse(response.body);
+    const { title, options } = createPollBody.parse(response.body);
 
     const poll = await prisma.poll.create({
       data: {
         title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return { title: option };
+            }),
+          },
+        },
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+            createdAt: true,
+          },
+        },
       },
     });
 
